72feddf1dff694847fe0a5ebb916723db975e0fb
RonnKarimi <ronnmwangi17@gmail.com>
2026-07-28 14:45:12 +0300

    Delete js directory

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = "todos";
-
-export function saveTodos(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-}
-
-export function loadTodos() {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
-}
